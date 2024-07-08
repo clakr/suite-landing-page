613e58db8e91743d6414c4746054d10939d85863
clakr <clarktolosa@gmail.com>
2024-07-08 13:04:18 +0800

    added assets and created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export default function App(){
-    return <>hello world!</>
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Marketing from "./components/Marketing";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Marketing />
+      <Footer />
+    </>
+  );
 }
