2609ec2c1a08ef660b744b633f4fe29e08d68649
clakr <clarktolosa@gmail.com>
2024-07-08 13:38:58 +0800

    `<Header />` component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,13 @@
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header className="desktop:max-w-[111rem] desktop:py-20 tablet:max-w-[68.9rem] tablet:mx-auto tablet:py-16 tablet:px-0 flex items-center justify-between px-[1.6rem] py-[2.4rem]">
+      <img src="./logo.svg" alt="" />
+      <button
+        type="button"
+        className="border-midnight tablet:text-[1.6rem] tablet:leading-[3.2rem] tablet:tracking-[.016rem] hover:bg-midnight hover:text-cream rounded-[.6rem] border px-[1.6rem] py-[1.2rem] text-[1.4rem] font-bold leading-[2.4rem] tracking-[.014rem]"
+      >
+        Request Beta Access
+      </button>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,72 @@
+import plugin from "tailwindcss/plugin";
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      midnight: "#172339",
+      slate: "#49566D",
+      cream: "#F3EDE7",
+      linen: "#FAF8F6",
+      lavender: "#A060FF",
+      magenta: "#CB30E3",
+      sunset: "#FFA84E",
+    },
+    screens: {
+      tablet: "768px",
+      desktop: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Epilogue", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".font-preset-xl": {
+          "font-weight": "700",
+          "font-size": "7.2rem",
+          "line-height": "7.8rem",
+          "letter-spacing": "-.1rem",
+        },
+        ".font-preset-l": {
+          "font-weight": "700",
+          "font-size": "4.8rem",
+          "line-height": "5.6rem",
+          "letter-spacing": "-.05rem",
+        },
+        ".font-preset-m": {
+          "font-weight": "700",
+          "font-size": "1.8rem",
+          "line-height": "3.2rem",
+          "letter-spacing": "-.02rem",
+        },
+        ".font-preset-s": {
+          "font-weight": "700",
+          "font-size": "1.6rem",
+          "line-height": "2.6rem",
+          "letter-spacing": "-2.5rem",
+        },
+        ".font-preset-quote": {
+          "font-size": "2rem",
+          "line-height": "3.5rem",
+          "letter-spacing": "0",
+        },
+        ".font-preset-body": {
+          "font-size": "1.8rem",
+          "line-height": "3.2rem",
+          "letter-spacing": "0",
+        },
+        ".font-preset-footer": {
+          "font-size": "1.5rem",
+          "line-height": "3.2rem",
+          "letter-spacing": "0",
+        },
+      });
+    }),
+  ],
+};
